bd9f64623a2fb6521d3287580e2d239bc8d3a29c
nicoschoutteet <84858608+nicoschoutteet@users.noreply.github.com>
2025-11-25 10:34:22 +0100

    Include SGM

--- R/JAOPuTo_helpers.R
+++ R/JAOPuTo_helpers.R
@@ -8,3 +8,13 @@ CoreBiddingZones <- tibble::tibble(
                   "Greece", "Montenegro", "North Macedonia", "Portugal", "Serbia", "Turkey", "Ukraine", "Kosovo",
                   "United Kingdom", "Morocco")
 )
+
+read_core_sgm <- function() {
+  path <- system.file("extdata", "core_sgm_6th_release.csv", package = "JAOPuTo")
+
+  if (path == "") {
+    stop("core_sgm_6th_release.csv has not been found in extdata of the JAOPuTo package.")
+  }
+
+  readr::read_csv(path, show_col_types = FALSE)
+}
